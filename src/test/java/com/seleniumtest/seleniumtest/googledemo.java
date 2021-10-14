fccfc9d86a00db4e25cd37f68e212fe40f1c6635
shravan258 <65703105+shravan258@users.noreply.github.com>
2021-10-14 21:44:04 +0530

    Update googledemo.java

--- src/test/java/com/seleniumtest/seleniumtest/googledemo.java
+++ src/test/java/com/seleniumtest/seleniumtest/googledemo.java
@@ -22,7 +22,7 @@ import org.testng.annotations.AfterTest;
 
 			@BeforeTest
 			public void beforeTest() {
-				System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
+				System.setProperty("webdriver.chrome.driver","D:\\chromedriver_win32\\chromedriver.exe");
 			 driver = new ChromeDriver();
 			}
 
